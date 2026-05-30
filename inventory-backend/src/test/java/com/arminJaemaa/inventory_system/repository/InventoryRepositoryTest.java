0ba19caad7903d601eae5b1cc0ccea76839a893f
ArminJaemaa <ajaemaa@tlu.ee>
2026-05-30 20:49:38 +0300

    test: add inventoryRepository testcase

---
+++ inventory-backend/src/test/java/com/arminJaemaa/inventory_system/repository/InventoryRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.arminJaemaa.inventory_system.repository;
+
+import com.arminJaemaa.inventory_system.entity.Product;
+import com.arminJaemaa.inventory_system.entity.Warehouse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import java.math.BigDecimal;
+
+@DataJpaTest
+public class InventoryRepositoryTest {
+
+    @Autowired
+    private InventoryRepository inventoryRepository;
+
+    @Autowired
+    private WarehouseRepository warehouseRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    public void findProductFromWarehouse() {
+        Warehouse warehouse = Warehouse.builder()
+                .name("Tallinn Logistics Hub")
+                .location("Tallinn, Estonia")
+                .build();
+        Warehouse savedWarehouse = warehouseRepository.save(warehouse);
+
+        Product product = Product.builder()
+                .name("iphone 16")
+                .sku("IPHONE-16")
+                .price(new BigDecimal(800.00))
+                .build();
+
+        Product savedProduct = productRepository.save(product);
+
+        Inventory inventory = inventory.build()
+                .warehouse(savedWarehouse)
+                .product(savedProduct)
+                .quantity(100)
+                .build();
+
+        Inventory savedInventory = inventoryRepository.save(inventory);
+
+        assertNotNull(savedInventory.getId());
+        assertEquals(100, savedInventory.getQuantity());
+        assertEquals("Iphone 16", savedInventory.getProduct().getName());
+    }
+}
